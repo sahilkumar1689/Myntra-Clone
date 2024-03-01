d393ae1eee89865f58e29ba3df3b522675015e1d
Sahil Kumar <Sahilkumar32755@gmail.com>
2024-03-02 01:17:34 +0530

    Add JavaScript

---
+++ folder1/newScript.js
@@ -0,0 +1,46 @@
+let arr;
+onLoad();
+function onLoad(){
+    arr = JSON.parse(localStorage.getItem("itemIndex")) || new Array();
+    setItems();
+    showBagCount();
+}
+
+function showBagCount(){
+    let bagCount = document.querySelector(".bagItemsNumber");
+    if(arr.length>0){
+        bagCount.style.display = "block";
+        bagCount.innerHTML=arr.length;
+    }else{
+        bagCount.style.display="none";
+    }
+}
+
+function addItems(index){
+    arr.push(index);
+    localStorage.setItem("itemIndex",JSON.stringify(arr));
+    showBagCount();
+}
+
+function setItems(){
+    let container = document.querySelector(".container");
+    let html="";
+    items.forEach((element,index)=>{
+        html += `
+        <div class="item">
+        <img src="${element.image}" alt="item1 image">
+        <div class="rating">
+            ${element.rating.stars} * | ${element.rating.count}
+        </div>
+        <div class="companyName">${element.company}</div>
+        <div class="tittleName">${element.item_name}</div>
+        <div class="price">
+            <span class="currentPrice">Rs. ${element.current_price}</span>
+            <span class="originalPrice">${element.original_price}</span>
+            <span class="discount">(${element.discount_percentage}% OFF)</span>
+        </div>
+        <button id=${index} class="addBtn" onclick="addItems(this.id)">Add To Bag</button>
+        </div>`;
+    })
+    container.innerHTML=html;
+}
